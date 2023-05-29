7bb51f086117ba995bac1c3392dad8adabf1969f
wuddz-devs <wuddz_devs@protonmail.com>
2023-05-29 19:29:29 -0400

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,53 @@
+<h1 align="center">Wuddz String/Mac Address Permutations/Combinations Generator</h1>
+
+## Description
+ - A Simple Script That Generates All/Non-Repeat Character Permutations/Combinations Of String Or Mac Addresses With Prefix/Suffix Partial Address
+ - Of Note Uses Lazy Generators, Will Take Some Time To Generate Huge Lists But Won't Be Memory Intensive
+ - Honestly Wrote This Quick Script As I Needed Some Of It's Implementation, Thought I'd Share.
+
+## Prerequisites
+ - Python : 3.7
+
+## Installation
+Install using [PyPI](https://pypi.org/project/wuddz-perms):
+```
+$ pip install wuddz-perms
+```
+Install locally by cloning or downloading and extracting the repo, then cd into 'dist' directory and execute:
+```
+$ pip install wuddz_perms-1.0.0.tar.gz
+```
+Then to run it, execute the following in the terminal:
+```
+$ wudz-perms
+```
+
+### Usage
+Generate All Combinations Of String To Default "output.txt" File.
+```
+$ wudz-perms -all -a wuddzdevs
+```
+Generate All Lower & Upper Case Permutations Of String To Default "output.txt" File.
+```
+$ wudz-perms -upl -a pythondev
+```
+Generate All Non Repeat Character Permutations Of String To Default "output.txt" File.
+```
+$ wudz-perms -nrc -a talented
+```
+Generate Mac Addresses With Specified Prefix/Suffix To Default "output.txt" File.
+```
+$ wudz-perms -mac -p 00:1a:65:ff
+```
+
+## Contact Info:
+ - Email:     wuddz_devs@protonmail.com                                                              
+ - Github:    https://github.com/wuddz-devs                                                          
+ - Telegram:  https://t.me/wuddz_devs
+ - Youtube:   https://youtube.com/@wuddz-devs
+ - Reddit:    https://reddit.com/user/wuddz-devs
+
+### Buy Me A Coffee!!
+ - ERC20 Address: 0x1F1C47dD653Af628D394eac7bAA9Ccf774fd784f
+
+#### Peace & Love Always!!
---
+++ wudz_perms.py
@@ -0,0 +1,72 @@
+import argparse, re, sys
+from os import system
+from itertools import product, permutations
+system('')
+
+
+def all_uplow_perms(pw):
+    for w in map(''.join, product(*zip(pw.upper(), pw.lower()))):
+        yield str(w)
+
+def all_norep_perms(pw,pl):
+    for w in map(''.join, permutations(pw, int(pl))):
+        yield str(w)
+
+def all_perms(pw,pl):
+    for w in map(''.join, product(pw, repeat=int(pl))):
+        yield str(w)
+
+def all_mac_perms(pre,suf,ml):
+    for m in map(''.join, product("ABCDEF1234567890", repeat=int(ml))):
+        yield str((':'.join(re.findall('.{1,2}', pre+m+suf))).upper())
+
+def mac_data(m):
+    if ':' in m:m = ''.join(str(m).split(':'))
+    return m, 12-len(m)
+
+def mac_setup(pre,suf):
+    for i in ['pre','suf']:
+        try:
+            ms, ml = mac_data(eval(i))
+            if ms and ml < 12:return ms,ml,i
+        except:pass
+
+def main(args):
+    md = ''
+    with open(args.output, 'a', encoding='utf-8') as fw:
+        mc = {'pre':args.prefix,'suf':args.suffix}
+        try:
+            if args.all or args.norep and args.length is None:args.length = len(args.string)
+            if args.all:md = all_perms(args.string,args.length)
+            elif args.mac:
+                ms, ml, mi = mac_setup(args.prefix,args.suffix)
+                mc[str(mi)] = ms
+                md = all_mac_perms(mc['pre'],mc['suf'],ml)
+                mc = {'pre':'','suf':''}
+            elif args.uplow:md = all_uplow_perms(args.string)
+            elif args.norep:md = all_norep_perms(args.string,args.length)
+        except:pass
+        if md:
+            c = 0
+            print("\033[1;37;40m[*] Generating Combinations...\033[0m")
+            while True:
+                try:
+                    fw.write('{}{}{}\n'.format(mc['pre'],next(md),mc['suf']))
+                    c += 1
+                except:break
+            print("\033[1;32;40m[+] Saved {} Combinations To {}\033[0m".format(c,args.output))
+        else:print("\033[1;31;40m[-] Error Occurred\033[0m")
+
+def cli_main():
+    parser = argparse.ArgumentParser(add_help = True, description ="Generate All/Non-Repeat Character Permutations/Combinations Of String Or Mac Addresses With Prefix/Suffix Partial Address.")
+    parser.add_argument('-a', '--string', type=str, default=None, help='String To Generate All Permutations/Combinations.')
+    parser.add_argument('-p', '--prefix', type=str, default='', help='String To Prefix Each Generated Permutation With.')
+    parser.add_argument('-s', '--suffix', type=str, default='', help='String To Suffix Each Generated Permutation With.')
+    parser.add_argument('-o', '--output', type=str, default='perms_output.txt', help='Output File To Save Generated Permutations To.')
+    parser.add_argument('-l', '--length', type=int, default=None, help='Integer Length Of Characters To Be Generated.')
+    parser.add_argument('-all', '--all', action='store_true', help='Generate All Combinations Of String.')
+    parser.add_argument('-upl', '--uplow', action='store_true', help='Generate All Lower & Upper Case Permutations Of String.')
+    parser.add_argument('-nrc', '--norep', action='store_true', help='Generate All Non Repeat Character Permutations Of String.')
+    parser.add_argument('-mac', '--mac', action='store_true', help='Generate Mac Addresses With Specified Prefix/Suffix.')
+    if len(sys.argv)==1:parser.print_help()
+    else:main(parser.parse_args())
